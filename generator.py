64346f6fd890e3ec0da4899735e933be65974d21
Itamar Hadad <itamar108@gmail.com>
2024-04-25 18:02:30 +0300

    baseline - defining Grammar.json, simple first case (single choice)

--- generator.py
+++ generator.py
@@ -7,38 +7,39 @@ import random
 with open('Grammar.json', 'r') as file:
     grammar = json.load(file)
 
-
-def create_directed_path_graph(n):
-    # Create a directed path graph with n vertices
-    G = nx.path_graph(n, create_using=nx.DiGraph)
-    return G
-
-
-def draw_graph(G):
-    # Draw the graph
-    nx.draw(G, with_labels=True, node_size=500, node_color='skyblue', font_size=12, font_weight='bold', arrows=True)
-    plt.show()
+class Generator:
+    def create_directed_path_graph(self, n):
+        # Create a directed path graph with n vertices
+        G = nx.path_graph(n, create_using=nx.DiGraph)
+        return G
 
 
-def get_ordered_clause(a, b):
-    return f"{b} came after {a}."
+    def draw_graph(self, G):
+        # Draw the graph
+        nx.draw(G, with_labels=True, node_size=500, node_color='skyblue', font_size=12, font_weight='bold', arrows=True)
+        plt.show()
 
 
-def generate_sentence_from_graph(G):
-    names = random.sample(grammar["names"], G.number_of_nodes())
-    all_edges = list(G.edges())
-    # with some probability - do shuffle the all_edges
-    random.shuffle(all_edges)
-    sentence = ""
-    for edge in all_edges:
-        sentence += get_ordered_clause(names[edge[0]], names[edge[1]]) + " "
-    return sentence[:-1]
+    def get_ordered_clause(self, a, b):
+        return f"{b} came after {a}."
+
+
+    def generate_sentence_from_graph(self, G):
+        names = random.sample(grammar["names"], G.number_of_nodes())
+        all_edges = list(G.edges())
+        # with some probability - do shuffle the all_edges
+        random.shuffle(all_edges)
+        sentence = ""
+        for edge in all_edges:
+            sentence += self.get_ordered_clause(names[edge[0]], names[edge[1]]) + " "
+        return sentence[:-1]
 
 
 # Define the number of vertices
 N = 6
 
 # Create the directed path graph
-directed_path_graph = create_directed_path_graph(N)
-draw_graph(directed_path_graph)
-print(generate_sentence_from_graph(directed_path_graph))
+generator = Generator()
+directed_path_graph = generator.create_directed_path_graph(N)
+generator.draw_graph(directed_path_graph)
+print(generator.generate_sentence_from_graph(directed_path_graph))
