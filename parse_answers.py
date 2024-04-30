197e5e84434447a72755fc7b2810b92e618342c4
rseck <esquirar@gmail.com>
2024-04-30 17:54:42 +0300

    parsed number of possibilites answers

--- parse_answers.py
+++ parse_answers.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 
 def sort_possible_arrangements():
@@ -17,3 +18,53 @@ def sort_possible_arrangements():
             json.dump(new_arrangements, json_file)
 
 
+number_pattern_1 = r"The answer is (\d+)"
+
+
+def parse_answers_if_possible():
+    queries_data_file = 'OUTPUTTED_PROMPTS/queries_open_questions.json'
+    with open(queries_data_file, 'r') as json_file:
+        queries_data = json.load(json_file)
+    results_data_file = 'results_from_models/gemma_7b_results_open_questions.json'
+    with open(results_data_file, 'r') as json_file:
+        results_data = json.load(json_file)
+    full_answers = results_data['model_full_answers']
+    prompt_types = queries_data['prompt_type']
+    parsed_answers = []
+    i = -1
+    for answer, prompt_type in zip(full_answers, prompt_types):
+        i += 1
+        if "NUMBER" in prompt_type:
+            match = re.search(number_pattern_1, answer)
+            if match:
+                parsed = match.group(1)
+                if len(parsed) < 3:
+                    parsed_answers.append(parsed)
+        else:
+            parsed_answers.append(f"UNPARSEBLE on index {i}")
+    results_data['parsed_answers'] = parsed_answers
+    with open(results_data_file, "w") as json_file:
+        json.dump(results_data, json_file)
+
+
+def iterate_gold_answers():
+    queries_data_file = 'OUTPUTTED_PROMPTS/queries_open_questions.json'
+    with open(queries_data_file, 'r') as json_file:
+        queries_data = json.load(json_file)
+    gold_answers = queries_data['expected_answer']
+    results_data_file = 'results_from_models/gemma_7b_results_open_questions.json'
+    with open(results_data_file, 'r') as json_file:
+        results_data = json.load(json_file)
+    parsed_answers = results_data['parsed_answers']
+    full_answers = results_data['model_full_answers']
+    for gold_answer, parsed_result, full_answer in zip(gold_answers, parsed_answers, full_answers):
+        if "UNPARSEBLE" in parsed_result:
+            print("****************************")
+            print(parsed_result)
+            print(f"gold:{gold_answer}")
+            print("received \n")
+            print(full_answer)
+            i = 1
+
+
+iterate_gold_answers()
